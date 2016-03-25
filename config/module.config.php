4ebd64a37d23123a40ea5e503b5ba82455aa0d1f
Aurélien Praga <aurelien.praga@inovia-team.com>
2016-03-25 15:05:18 +0100

    Changes for CKFinder 3

--- config/module.config.php
+++ config/module.config.php
@@ -3,30 +3,15 @@
 return array(
     'router' => array(
         'routes' => array(
-
             'ckfinder' => array(
                 'type' => 'literal',
                 'options' => array(
-                    'route'    => '/ckfinder',
+                    'route'    => '/ckfinder/core/connector/php/connector.php',
                     'defaults' => array(
-                        'controller' => 'CKFinderModule\Controller\Browse',
+                        'controller' => 'CKFinderModule\Controller\Connector',
                         'action'     => 'index',
                     ),
-                ),
-                'may_terminate' => true,
-
-                'child_routes' => array(
-                    'connector' => array(
-                        'type'    => 'literal',
-                        'options' => array(
-                            'route'    => '/core/connector/php/connector.php',
-                            'defaults' => array(
-                                'controller' => 'CKFinderModule\Controller\Connector',
-                                'action'     => 'index',
-                            ),
-                        ),
-                    ),
-                ),
+                )
 
             ),
 
@@ -41,48 +26,13 @@ return array(
 
     'controllers' => array(
         'invokables' => array(
-            'CKFinderModule\Controller\Browse' => 'CKFinderModule\Controller\BrowseController',
             'CKFinderModule\Controller\Connector' => 'CKFinderModule\Controller\ConnectorController'
-        ),
-    ),
-
-    /**
-     * Default BjyAuthorize configuration for ACL
-     */
-    'bjyauthorize' => array(
-        'guards' => array(
-            'BjyAuthorize\Guard\Route' => array(
-                array('route' => 'ckfinder', 'roles' => array('admin')),
-                array('route' => 'ckfinder/connector', 'roles' => array('admin')),
-            ),
-        ),
-    ),
-
-    /**
-     * Default ZfcRbac configuration for RBAC
-     */
-    'zfcrbac' => array(
-        'firewall_route' => true,
-        'firewalls' => array(
-            'ZfcRbac\Firewall\Route' => array(
-                'zfcadmin' => array('route' => '^ckfinder/*', 'roles' => 'admin')
-            )
         ),
     ),
 
     'ckfinder_module' => array(
         'ckfinder_vendor_path'  => './vendor/ckfinder/ckfinder/',
         'ckfinder_config_path'  => __DIR__ . '/ckfinder.config.php',
-        'ckfinder_basepath'     => '/ckfinder/', // should be the same as the folder made aaccessible with the assetmanager
-        'ckfinder_width'        => '100%',
-        'ckfinder_height'       => '100%',
-    ),
-
-    'asset_manager' => array(
-        'resolver_configs' => array(
-            'paths' => array(
-                realpath('./vendor/ckfinder/'), // this way example.com/ckfinder is accessible
-            ),
-        ),
+        'ckfinder_basepath'     => '/ckfinder/', // should be the same as the folder made accessible with the assetmanager
     ),
 );
--- src/CKFinderModule/Controller/ConnectorController.php
+++ src/CKFinderModule/Controller/ConnectorController.php
@@ -9,87 +9,12 @@ class ConnectorController extends AbstractActionController
     {
         $config = $this->getServiceLocator()->get('CKFinderModule\Config');
 
-        $ckFinderPath = realpath($config['ckfinder_vendor_path']).'/';
-        $ckFinderConnectorPath = $ckFinderPath.'core/connector/php/';
-        $ckFinderConfigPath = realpath($config['ckfinder_config_path']);
+        require_once realpath($config['ckfinder_vendor_path']) . '/core/connector/php/vendor/autoload.php';
 
+        $ckfinder = new \CKSource\CKFinder\CKFinder($config['ckfinder_config_path']);
 
-        // Below is basically a copy from connector.php:
+        $ckfinder->run();
 
-        /**
-         * Protect against sending content before all HTTP headers are sent (#186).
-         */
-        ob_start();
-
-        /**
-         * define required constants
-         */
-        require_once $ckFinderConnectorPath . '/constants.php';
-
-        // @ob_end_clean();
-        // header("Content-Encoding: none");
-
-        /**
-         * we need this class in each call
-         */
-        require_once CKFINDER_CONNECTOR_LIB_DIR . '/CommandHandler/CommandHandlerBase.php';
-        /**
-         * singleton factory
-         */
-        require_once CKFINDER_CONNECTOR_LIB_DIR . '/Core/Factory.php';
-        /**
-         * utils class
-         */
-        require_once CKFINDER_CONNECTOR_LIB_DIR . '/Utils/Misc.php';
-        /**
-         * hooks class
-         */
-        require_once CKFINDER_CONNECTOR_LIB_DIR . '/Core/Hooks.php';
-
-        $utilsSecurity =& \CKFinder_Connector_Core_Factory::getInstance("Utils_Security");
-        $utilsSecurity->getRidOfMagicQuotes();
-
-        /**
-         * $config must be initialised
-         */
-        $config = array();
-        $config['Hooks'] = array();
-        $config['Plugins'] = array();
-
-        /**
-         * Fix cookies bug in Flash.
-         */
-        if (!empty($_GET['command']) && $_GET['command'] == 'FileUpload' && !empty($_POST)) {
-            foreach ($_POST as $key => $val) {
-                if (strpos($key, "ckfcookie_") === 0) {
-                    $_COOKIE[str_replace("ckfcookie_", "", $key)] = $val;
-                }
-            }
-        }
-
-        /**
-         * read config file
-         */
-        require_once $ckFinderConfigPath;
-
-        \CKFinder_Connector_Core_Factory::initFactory();
-        $connector =& \CKFinder_Connector_Core_Factory::getInstance("Core_Connector");
-
-        // The globals array is being used for the config and connector
-        // ( and $GLOBALS['CKFLang'], which is set by ckfinder itself )
-        // we are however not in a global context right now, so we're going to
-        // have to add the keys/references manually
-        $GLOBALS['config'] =& $config;
-        $GLOBALS['connector'] =& $connector;
-
-        // Now that the config is setup like it would be in a normal ckfinder run:
-        if (isset($_GET['command'])) {
-            $connector->executeCommand($_GET['command']);
-        } else {
-            $connector->handleInvalidCommand();
-        }
-
-        // Please do not render view / layout!
         return $this->response;
     }
 }
